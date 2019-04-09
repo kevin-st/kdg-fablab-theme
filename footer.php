0a0edba1b8e2a39f1b53b31ea6d0eb312c53b6f1
Katodebock <36281116+Katodebock@users.noreply.github.com>
2019-04-09 16:35:33 +0200

    Max size header & footer

--- footer.php
+++ footer.php
@@ -1,5 +1,18 @@
     <footer>
       <?php /* footer content goes here */ ?>
+      <div id="adres">
+        <h1>KdG Fablab</h1>
+        <p>Salesianenlaan 90</p>
+        <p>2660 Hoboken</p>
+      </div>
+      <div id="socials">
+        <a href="https://www.facebook.com/kareldegrotehogeschool/">
+          <img src="<?php echo get_theme_file_uri("img/fbWhite.png"); ?>" alt="facebook icon">
+        </a>
+        <a href="https://www.instagram.com/kdghogeschool/?hl=nl">
+          <img src="<?php echo get_theme_file_uri("img/igWhite.png"); ?>" alt="instagram icon">
+        </a>
+      </div>
     </footer>
     <?php wp_footer(); ?>
   </body>
