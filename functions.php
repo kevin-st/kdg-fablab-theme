84b9d008bfa4fd8d405cbd3e4ddc6b35379e7591
kevin-st <kevin.stollaerd@gmail.com>
2019-04-10 11:33:18 +0200

    Fixed: avoid caching

--- functions.php
+++ functions.php
@@ -22,9 +22,9 @@
         wp_enqueue_style('font-awesome', ''//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'');
      */
 
-     wp_enqueue_style('kdg_fablab_main_styles', get_stylesheet_uri());
+     wp_enqueue_style('kdg_fablab_main_styles', get_stylesheet_uri(), NULL, microtime());
 
-     wp_enqueue_script('kdg_fablab_js', get_theme_file_uri('js/scripts-bundled.js'), NULL, '1.0', true);
+     wp_enqueue_script('kdg_fablab_js', get_theme_file_uri('js/scripts-bundled.js'), NULL, microtime()/*'1.0'*/, true);
    }
 
    /**
