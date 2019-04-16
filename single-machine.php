4bf300ace6e020a453221771f66701485f20b770
kevin-st <kevin.stollaerd@gmail.com>
2019-04-16 18:45:55 +0200

    Basic template toestel

--- single-machine.php
+++ single-machine.php
@@ -6,12 +6,34 @@
     in this case: a post with type of machine
   */
 ?>
-<h1><?php the_title(); ?></h1>
-<h1><?php the_title(); ?></h1>
-<h1><?php the_title(); ?></h1>
-<h1><?php the_title(); ?></h1>
-<h1><?php the_title(); ?></h1>
-<h1><?php the_title(); ?></h1>
+<main>
+  <?php
+    while(have_posts()) {
+      the_post();
+      $title = strtolower(str_replace(" ", "-", get_the_title()));
+  ?>
+  <h1 role="title"><?php the_title(); ?></h1>
+  <div role="thumbnail">
+    <?php
+      if (has_post_thumbnail()) {
+        echo the_post_thumbnail();
+      } else {
+    ?>
+    <img src="<?php echo get_theme_file_uri("img/default_machine.jpg"); ?>" alt="machine"/>
+    <?php
+      }
+    ?>
+  </div>
+  <div role="content">
+    <p><?php the_content(); ?></p>
+  </div>
+  <div role="button">
+    <a href="<?php echo esc_url(add_query_arg("machine", $title, site_url('/reserveren'))); ?>">Reserveren</a> <!-- Moet nog nagekeken worden, reservatie plugin -->
+  </div>
+  <?php
+    }
+  ?>
+</main>
 <?php
   get_footer();
 
