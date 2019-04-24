9fbccdd1503f284107d37bb746e7cf133a14fdaf
Katodebock <36281116+Katodebock@users.noreply.github.com>
2019-04-24 17:06:57 +0200

    Frontpage edit

--- front-page.php
+++ front-page.php
@@ -10,92 +10,90 @@
     <img id="frontpageImg" src="<?php echo get_theme_file_uri("img/fablab.PNG"); ?>" alt="fablab workarea">
     <a class="introbtn" href="<?php echo site_url('login') ?>">Kom Binnen</a>
 
-  <main id="mainFrontpage">
+  <main id="mainFrontpage" class="disp-f">
     <section role="news">
       <h1>Laatste nieuws</h1>
+      <article class="disp-f">
+        <?php
+          $homepage_news = new WP_Query([
+            "posts_per_page" => 1 // control number of posts with this -> -1 is all posts
+          ]);
 
-      <?php
-        $homepage_news = new WP_Query([
-          "posts_per_page" => 1 // control number of posts with this -> -1 is all posts
-        ]);
+          while($homepage_news->have_posts()) {
+            $homepage_news->the_post();
+        ?>
 
-        while($homepage_news->have_posts()) {
-          $homepage_news->the_post();
-      ?>
-      <div class="machine-item small"> <!-- change classes if needed -->
-        <?php if (has_post_thumbnail()) { ?>
-         <div class="thumbnail">
-           <?php the_post_thumbnail(); ?>
-         </div>
-         <?php } else { ?>
-         <img class="thumbnail" src="<?php echo get_theme_file_uri("img/default_news.jpg"); ?>" alt="news">
-        <?php } ?>
-        <div class="infocontainer">
-        <h2 class="title"><?php the_title(); ?></h2>
+          <?php if (has_post_thumbnail()) { ?>
+           <div class="thumbnail">
+             <?php the_post_thumbnail(); ?>
+           </div>
+           <?php } else { ?>
+           <img class="thumbnail" src="<?php echo get_theme_file_uri("img/default_news.jpg"); ?>" alt="news">
+          <?php } ?>
+          <div class="content">
+            <h2 class="title"><?php the_title(); ?></h2>
 
-         <p class="excerpt">
-          <?php
-            if (has_excerpt()) {
-              echo get_the_excerpt();
-            } else {
-              echo wp_trim_words(get_the_content(), 40); // control number of words
-            }
-          ?>
-         </p>
-         <div class="btnCont">
-           <a class="btn" href="<?php the_permalink(); ?>">Lees verder</a>
-           <a class="btn" href="<?php echo get_permalink(get_option("page_for_posts")); ?>">Meer nieuws</a>
-         </div>
+             <p class="excerpt">
+              <?php
+                if (has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 40); // control number of words
+                }
+              ?>
+             </p>
+             <div class="buttons ">
+               <a class="btn btn-dark" href="<?php the_permalink(); ?>">Lees verder</a>
+               <a class="btn btn-dark" href="<?php echo get_permalink(get_option("page_for_posts")); ?>">Meer nieuws</a>
+             </div>
 
-         </div>
-      </div>
-      <?php
-        }
-      ?>
+           </div>
+
+        <?php
+          }
+        ?>
+      </article>
     </section>
     <section role="machines">
       <h1>Onze toestellen</h1>
-      <?php
-        $last_added_machine = new WP_Query([
-          "posts_per_page" => 1, // control number of posts with this -> -1 is all posts
-          "post_type" => "machine"
-        ]);
+      <article class="disp-f">
+        <?php
+          $last_added_machine = new WP_Query([
+            "posts_per_page" => 1, // control number of posts with this -> -1 is all posts
+            "post_type" => "machine"
+          ]);
 
-        while($last_added_machine->have_posts()) {
-          $last_added_machine->the_post();
-      ?>
-      <div class="machine-item small"> <!-- change classes if needed -->
+          while($last_added_machine->have_posts()) {
+            $last_added_machine->the_post();
+        ?>
+           <?php if (has_post_thumbnail()) { ?>
+            <div class="thumbnail">
+              <?php the_post_thumbnail(); ?>
+            </div>
+            <?php } else { ?>
+            <img class="thumbnail" src="<?php echo get_theme_file_uri("img/default_machine.jpg"); ?>" alt="machine">
+           <?php } ?>
+           <div class="content">
+           <h2 class="title"><?php the_title(); ?></h2>
+           <p class="excerpt">
+            <?php
+              if (has_excerpt()) {
+                echo get_the_excerpt();
+              } else {
+                echo wp_trim_words(get_the_content(), 40); // control number of words
+              }
+            ?>
+           </p>
+           <div class="buttons">
+             <a class="btn btn-dark" href="<?php the_permalink(); ?>">Reserveer me</a>
+             <a class="btn btn-dark" href="<?php echo get_post_type_archive_link("machine"); ?>">Meer toestellen</a>
+           </div>
 
-         <?php if (has_post_thumbnail()) { ?>
-          <div class="thumbnail">
-            <?php the_post_thumbnail(); ?>
-          </div>
-          <?php } else { ?>
-          <img class="thumbnail" src="<?php echo get_theme_file_uri("img/default_machine.jpg"); ?>" alt="machine">
-         <?php } ?>
-         <div class="infocontainer">
-
-
-         <h2 class="title"><?php the_title(); ?></h2>
-         <p class="excerpt">
-          <?php
-            if (has_excerpt()) {
-              echo get_the_excerpt();
-            } else {
-              echo wp_trim_words(get_the_content(), 40); // control number of words
-            }
-          ?>
-         </p>
-         <div class="btnCont">
-           <a class="btn" href="<?php the_permalink(); ?>">Reserveer me</a>
-           <a class="btn" href="<?php echo get_post_type_archive_link("machine"); ?>">Meer toestellen</a>
-         </div>
-
-         </div>
-      </div>
-      <?php
-        }
-      ?>
+           </div>
+        <?php
+          }
+        ?>
+      </article>
     </section>
 
 
