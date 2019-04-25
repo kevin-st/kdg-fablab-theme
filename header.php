b15cb5f35601834e50c7cafb8a19cc5b3f2607d8
Katodebock <36281116+Katodebock@users.noreply.github.com>
2019-04-25 20:11:53 +0200

    Header changes

--- header.php
+++ header.php
@@ -7,24 +7,27 @@
   </head>
   <body <?php body_class(); ?>>
     <header>
-      <div id="vormgeving"></div>
-      <div id="vormgeving2"></div>
-      <div id="logonaam">
-        <a href="<?php echo site_url(); ?>">
-          <img src="<?php echo get_theme_file_uri("img/logowit.png"); ?>" alt="fablab icon">
-          <h1>FabLab</h1>
-        </a>
-      </div>
-      <div class="burgerbtn">
-        <img id="menuimg" src="<?php echo get_theme_file_uri("img/onder.png"); ?>" alt="menu icon">
-        <img id="menuimg2" class="zichtbaarheid" src="<?php echo get_theme_file_uri("img/boven.png"); ?>" alt="menu icon">
-      </div>
-      <div id="menu-container" class="zichtbaarheid">
-        <?php
-           wp_nav_menu([
-             'theme_location' => 'main_navigation'
-           ]);
-        ?>
-      </div>
+      <nav>
+        <div id="vormgeving"></div>
+        <div id="vormgeving2"></div>
+        <div id="logonaam">
+          <a href="<?php echo site_url(); ?>">
+            <img src="<?php echo get_theme_file_uri("img/logowit.png"); ?>" alt="fablab icon">
+            <h1>FabLab</h1>
+          </a>
+        </div>
+        <div class="burgerbtn">
+          <img id="menuimg" src="<?php echo get_theme_file_uri("img/onder.png"); ?>" alt="menu icon">
+          <img id="menuimg2" class="zichtbaarheid" src="<?php echo get_theme_file_uri("img/boven.png"); ?>" alt="menu icon">
+        </div>
+        <div id="menu-container" class="zichtbaarheid">
+          <?php
+             wp_nav_menu([
+               'theme_location' => 'main_navigation'
+             ]);
+          ?>
+        </div>
+      </nav>
+
       <?php kdg_fablab_get_page_banner(); ?>
     </header>
