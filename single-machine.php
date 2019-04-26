645d9867017ed12d25c229d93f6fb2c9eac74e07
Katodebock <36281116+Katodebock@users.noreply.github.com>
2019-04-26 16:36:46 +0200

    detail toestel basic layout

--- single-machine.php
+++ single-machine.php
@@ -12,23 +12,29 @@
       the_post();
       $title = strtolower(str_replace(" ", "-", get_the_title()));
   ?>
-  <h1 role="title"><?php the_title(); ?></h1>
-  <div role="thumbnail">
-    <?php
-      if (has_post_thumbnail()) {
-        echo the_post_thumbnail();
-      } else {
-    ?>
-    <img src="<?php echo get_theme_file_uri("img/default_machine.jpg"); ?>" alt="machine"/>
-    <?php
-      }
-    ?>
+  <div class="introMachine">
+    <div role="thumbnail" class="thumbnail">
+      <?php
+        if (has_post_thumbnail()) {
+          echo the_post_thumbnail();
+        } else {
+      ?>
+      <img src="<?php echo get_theme_file_uri("img/default_machine.jpg"); ?>" alt="machine"/>
+      <?php
+        }
+      ?>
+    </div>
+    <div class="flexdiv">
+      <h1 role="title" class="title"><?php the_title(); ?></h1>
+    </div>
+
   </div>
-  <div role="content">
+  <div role="content" class="content">
     <p><?php the_content(); ?></p>
   </div>
-  <div role="button">
-    <a href="<?php echo esc_url(add_query_arg("machine", $title, site_url('/reserveren'))); ?>">Reserveren</a> <!-- Moet nog nagekeken worden, reservatie plugin -->
+  <div role="button" class="detailBtn">
+    <a class="btn btn-dark " href="<?php echo esc_url(add_query_arg("machine", $title, site_url('/reserveren'))); ?>">Reserveer dit toestel!</a> <!-- Moet nog nagekeken worden, reservatie plugin -->
+    <a class="btn btn-dark " href="<?php echo get_post_type_archive_link("machine"); ?>">Terug naar toestellen</a>
   </div>
   <?php
     }
