6549b7d5c08d783aa7c7bdb0eeda2d5859056523
T-rix <32762914+T-rix@users.noreply.github.com>
2019-04-30 04:01:08 +0200

    insta feed #kdgfablab

--- page-showcase.php
+++ page-showcase.php
@@ -16,23 +16,27 @@
 
 
 <body>
-<main>
+<main class="content">
 
     <h1>Showcase</h1>
     
-<!-- <div> tag needs to be here for now - don't delete -->
-<div id="curator-feed"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>   
- 
+    <p>Zoek je inspiratie? Kijk dan even hieronder.</p>
+    <p>Heb je zelf iets leuks gemaakt dat je wil delen? Zet je post dan op Instagram met <a href="https://www.instagram.com/explore/tags/kdgfablab/">#kdgfablab</a> en inspireer anderen.</p><br>
+    
+    <div class="feed">
+        <!-- <div>tag needs to be here for now - don't delete -->
+        <div id="curator-feed"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>   
 
-<!-- Javascript for the #kdgfablab in grid style -->
-<script type="text/javascript">
-/* curator-feed */
-(function(){
-var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
-i.src = "https://cdn.curator.io/published/123fbe1f-fe03-4bfd-881c-a8d4fe5092fa.js";
-e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
-})();
-</script>   
+        <!-- Javascript for the #kdgfablab in grid style -->
+        <script type="text/javascript">
+        /* curator-feed */
+        (function(){
+        var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
+        i.src = "https://cdn.curator.io/published/123fbe1f-fe03-4bfd-881c-a8d4fe5092fa.js";
+        e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+        })();
+        </script>   
+    </div>
 
 
    <!--
