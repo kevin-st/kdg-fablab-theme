c839a679a9966bfe69c73395c43abf57f0994b6e
T-rix <32762914+T-rix@users.noreply.github.com>
2019-04-30 02:27:36 +0200

    showcase JS code insta-feed

--- page-showcase.php
+++ page-showcase.php
@@ -14,11 +14,26 @@
    */
 ?>
 
+
+<body>
 <main>
 
     <h1>Showcase</h1>
     
-    
+<!-- <div> tag needs to be here for now - don't delete -->
+<div id="curator-feed"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>   
+ 
+
+<!-- Javascript for the #kdgfablab in grid style -->
+<script type="text/javascript">
+/* curator-feed */
+(function(){
+var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
+i.src = "https://cdn.curator.io/published/123fbe1f-fe03-4bfd-881c-a8d4fe5092fa.js";
+e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+})();
+</script>   
+
 
    <!--
     <div class="testDiv">
@@ -51,6 +66,7 @@
     -->
 
 </main>
+</body>
 
 <?php
   get_footer();
