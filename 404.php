0bcd9c9aeb422ce2cb22f507e54a7dbeb3c7d222
Katodebock <36281116+Katodebock@users.noreply.github.com>
2019-05-03 15:27:27 +0200

    error 404

--- 404.php
+++ 404.php
@@ -1,13 +1,16 @@
    <?php
    get_header(); ?>
 
-     <div class="intro404 disp-f">
-       <img class="img_404" src="<?php echo get_theme_file_uri("img/iconStop.png"); ?>">
-       <div class="introContent_404">
-         <h1 class="page-title">Error404: Helaas kunnen we  de pagina die u zoekt niet vinden.</h1>
-         <p>Dit kan komen doordat de pagina niet meer bestaat, of doordat eeen typefout is gemaakt bij het invoeren van het adres. U kunt het via de navigatie de weg vinden.</p>
-       </div>
-     </div>
+<main>
+  <div class="intro404 disp-f">
+    <img class="img_404" src="<?php echo get_theme_file_uri("img/iconStop.png"); ?>">
+    <div class="introContent_404">
+      <h1 class="page-title">Error404: Helaas kunnen we  de pagina die u zoekt niet vinden.</h1>
+      <p>Dit kan komen doordat de pagina niet meer bestaat, of doordat eeen typefout is gemaakt bij het invoeren van het adres. U kunt het via de navigatie de weg vinden.</p>
+    </div>
+  </div>
+</main>
+
 
 
 
