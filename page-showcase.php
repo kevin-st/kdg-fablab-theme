bd3c2d619381829ec287255a8f85866f19243224
carodeweze <caro_de_weze@hotmail.com>
2019-05-03 13:41:25 +0200

    commentaar verwijdert

--- page-showcase.php
+++ page-showcase.php
@@ -38,37 +38,6 @@
         </script>   
     </div>
 
-
-   <!--
-    <div class="testDiv">
-
-        <div class="imgShowcase1">
-
-            <img id="showcaseImg1" class="showcase" src="<//?php echo get_theme_file_uri("img/showcase1.png"); ?>" alt="showcase">
-
-            <img id="showcaseImg2" class="showcase" src="<//?php echo get_theme_file_uri("img/showcase2.png"); ?>" alt="showcase">
-
-        </div>
-
-        <div class="imgShowcase2">
-
-            <img id="showcaseImg3" class="showcase" src="<//?php echo get_theme_file_uri("img/showcase3.png"); ?>" alt="showcase">
-
-            <img id="showcaseImg4" class="showcase" src="<//?php echo get_theme_file_uri("img/showcase4.png"); ?>" alt="showcase">
-
-        </div>
-
-        <div class="imgShowcase3">
-
-            <img id="showcaseImg5" class="showcase" src="<//?php echo get_theme_file_uri("img/showcase5.png"); ?>" alt="showcase">
-
-            <img id="showcaseImg6" class="showcase" src="<//?php echo get_theme_file_uri("img/showcase6.png"); ?>" alt="showcase">
-
-        </div>
-
-    </div>
-    -->
-
 </main>
 </body>
 
