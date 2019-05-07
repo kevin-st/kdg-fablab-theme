b18b2d47f9828e48f07d0be64cabf50f286a988e
kevin-st <kevin.stollaerd@gmail.com>
2019-05-07 17:42:05 +0200

    Added: third step reservation process workshop (WIP)

--- page-reserveren.php
+++ page-reserveren.php
@@ -1,7 +1,8 @@
 <?php
   /* Template Name: Reserveren Template */
-  $current_step = "0";
+  $current_step = isset($_SESSION["reservation"]["reservation-step"]) ? $_SESSION["reservation"]["reservation-step"] : "0";
   $reservation_type = isset($_SESSION["reservation"]["reservation-type"]) ? $_SESSION["reservation"]["reservation-type"] : NULL;
+  $reservation_item = isset($_SESSION["reservation"]["reservation-item"]) ? $_SESSION["reservation"]["reservation-item"] : NULL;
 
   echo "<pre>";
   echo "SESSIE";
@@ -14,7 +15,6 @@
   echo "</pre>";
 
   if (isset($_POST["submit"])) {
-
     // When submit value is next
     if (strtolower($_POST["submit"]) === "volgende") {
       $next_step = isset($_POST["step"]) ? $_POST["step"] : NULL;
@@ -22,18 +22,22 @@
       // Initial step
       if ($current_step === "0") {
         if (isset($_POST["reservation-type"])) {
-          $_SESSION["reservation"]["reservation-type"] = $_POST["reservation-type"];
-          $reservation_type = $_SESSION["reservation"]["reservation-type"];
-          $current_step = $next_step;
+          $_SESSION["reservation"]["reservation-type"] = $reservation_type = $_POST["reservation-type"];
         }
-      }
-      // End of initial step
+      } // End of initial step
+      else if ($current_step === "1") {
+        if ($reservation_type === "workshop") {
+          $_SESSION["reservation"]["reservation-item"] = $reservation_item = $_POST["reservation-item"];
+        }
+      } // End of first step
 
+      // update the current step value
+      $_SESSION["reservation"]["reservation-step"] = $current_step = $next_step;
     }
     else {
-
+      // submit value is previous
+      $current_step -= 1;
     }
-
   }
 
   get_header();
@@ -57,7 +61,7 @@
         <!-- Initial step -->
         <div class="input-group">
           <label for="reservation-type">Wat wilt u reserveren?</label>
-          <select for="reservation-type" name="reservation-type">
+          <select id="reservation-type" for="reservation-type" name="reservation-type">
             <option value="">-- Kiezen --</option>
             <option value="machine">Toestel</option>
             <option value="workshop">Workshop</option>
@@ -69,24 +73,56 @@
       <?php } ?>
 
 
-      <?php echo "current step is: " . $current_step; ?>
+      <?php echo "De huidige stap is: " . $current_step; ?>
       <?php if ($current_step === "1" && $reservation_type === "workshop") { ?>
+      <!-- First step workshop -->
       <div class="reservation-info">
         <p>
           <span class="fw-b">Reservatie voor:</span>
           <?php echo $reservation_type; ?>
         </p>
       </div>
-      <!-- First step workshop -->
       <div class="input-group">
         <label for="reservation-item">Welke workshop wilt u reserveren?</label>
-        <select for="reservation-item">
+        <select id="reservation-item" name="reservation-item">
+          <?php
+            $all_workshops = new WP_Query([
+              "posts_per_page" => -1,
+              "post_type" => "workshop"
+            ]);
 
+            while($all_workshops->have_posts()) {
+              $all_workshops->the_post();
+              $title = get_the_title();
+          ?>
+          <option value="<?php echo $title; ?>">
+            <?php echo $title; ?>
+          </option>
+          <?php
+            }
+          ?>
         </select>
       </div>
+      <input type="hidden" name="step" value="3" />
+      <input class="btn btn-submit" name="submit" type="submit" value="Volgende" />
       <!-- End of first step workshop -->
       <?php } ?>
 
+
+      <!-- Third step workshop -->
+      <?php if ($current_step === "3" && $reservation_type === "workshop") { ?>
+        <div class="reservation-info">
+          <p>
+            <span class="fw-b">Reservatie voor:</span>
+            <?php echo $reservation_type; ?>
+          </p>
+          <p>
+            <span class="fw-b">Workshop:</span>
+            <?php echo $reservation_item; ?>
+          </p>
+        </div>
+      <?php } ?>
+      <!-- End of Third step workshop -->
 
     </form>
   </div>
