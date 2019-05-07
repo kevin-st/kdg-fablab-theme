b4d3949ed206d77a0c13ee1d7695bb4b29a2ee6e
kevin-st <kevin.stollaerd@gmail.com>
2019-05-07 18:43:26 +0200

    Added: error handling reservation init step workshop

--- page-reserveren.php
+++ page-reserveren.php
@@ -1,8 +1,11 @@
 <?php
   /* Template Name: Reserveren Template */
-  $current_step = isset($_SESSION["reservation"]["reservation-step"]) ? $_SESSION["reservation"]["reservation-step"] : "0";
+  $current_step = isset($_SESSION["reservation"]["reservation-step"]) ? intval($_SESSION["reservation"]["reservation-step"]) : 0;
   $reservation_type = isset($_SESSION["reservation"]["reservation-type"]) ? $_SESSION["reservation"]["reservation-type"] : NULL;
   $reservation_item = isset($_SESSION["reservation"]["reservation-item"]) ? $_SESSION["reservation"]["reservation-item"] : NULL;
+
+  // check if a certain step has errors
+  $init_step_error = "";
 
   echo "<pre>";
   echo "SESSIE";
@@ -17,22 +20,26 @@
   if (isset($_POST["submit"])) {
     // When submit value is next
     if (strtolower($_POST["submit"]) === "volgende") {
-      $next_step = isset($_POST["step"]) ? $_POST["step"] : NULL;
+      $next_step = isset($_POST["step"]) ? intval($_POST["step"]) : 0;
 
       // Initial step
-      if ($current_step === "0") {
-        if (isset($_POST["reservation-type"])) {
+      if ($current_step === 0) {
+        if (!empty($_POST["reservation-type"])) {
           $_SESSION["reservation"]["reservation-type"] = $reservation_type = $_POST["reservation-type"];
+        } else {
+          $init_step_error = "Maak een keuze";
         }
       } // End of initial step
-      else if ($current_step === "1") {
+      else if ($current_step === 1) {
         if ($reservation_type === "workshop") {
           $_SESSION["reservation"]["reservation-item"] = $reservation_item = $_POST["reservation-item"];
         }
       } // End of first step
 
-      // update the current step value
-      $_SESSION["reservation"]["reservation-step"] = $current_step = $next_step;
+      if (empty($init_step_error)) {
+        // update the current step value
+        $_SESSION["reservation"]["reservation-step"] = $current_step = $next_step;
+      }
     }
     else {
       // submit value is previous
@@ -57,15 +64,16 @@
   <div class="page-reserveren-content">
     <form id="reservation-form" action="<?php the_permalink(); ?>" method="post">
 
-      <?php if ($current_step === "0") { ?>
+      <?php if ($current_step === 0) { ?>
         <!-- Initial step -->
         <div class="input-group">
           <label for="reservation-type">Wat wilt u reserveren?</label>
-          <select id="reservation-type" for="reservation-type" name="reservation-type">
+          <select id="reservation-type" for="reservation-type" name="reservation-type" class="<?php echo (!empty($init_step_error)) ? "error" : "" ; ?>">
             <option value="">-- Kiezen --</option>
             <option value="machine">Toestel</option>
             <option value="workshop">Workshop</option>
           </select>
+          <span class="error-message <?php echo ($init_step_error !== "") ? 'disp-b' : 'disp-n'; ?>"><?php echo $init_step_error; ?></span>
         </div>
         <input type="hidden" name="step" value="1" />
         <input class="btn btn-submit" type="submit" name="submit" value="Volgende" />
@@ -74,7 +82,7 @@
 
 
       <?php echo "De huidige stap is: " . $current_step; ?>
-      <?php if ($current_step === "1" && $reservation_type === "workshop") { ?>
+      <?php if ($current_step === 1 && $reservation_type === "workshop") { ?>
       <!-- First step workshop -->
       <div class="reservation-info">
         <p>
@@ -110,7 +118,7 @@
 
 
       <!-- Third step workshop -->
-      <?php if ($current_step === "3" && $reservation_type === "workshop") { ?>
+      <?php if ($current_step === 3 && $reservation_type === "workshop") { ?>
         <div class="reservation-info">
           <p>
             <span class="fw-b">Reservatie voor:</span>
