d044e6544745c8e76a4e06141ac78cf46a157951
kevin-st <kevin.stollaerd@gmail.com>
2019-05-09 12:16:01 +0200

    Fix: error handling machine reservation process

--- page-reserveren.php
+++ page-reserveren.php
@@ -4,9 +4,10 @@
   $reservation_type = isset($_SESSION["reservation"]["reservation-type"]) ? $_SESSION["reservation"]["reservation-type"] : NULL;
   $reservation_item = isset($_SESSION["reservation"]["reservation-item"]) ? $_SESSION["reservation"]["reservation-item"] : NULL;
   $reservation_date = isset($_SESSION["reservation"]["reservation-date"]) ? $_SESSION["reservation"]["reservation-date"] : NULL;
+  $reservation_time_slots = isset($_SESSION["reservation"]["reservation-time-slots"]) ? $_SESSION["reservation"]["reservation-time-slots"] : [];
 
   // check if a certain step has errors
-  $init_step_error = $first_step_ws_error = $first_step_machine_error = $first_step_machine_date_error = "";
+  $init_step_error = $first_step_error = $first_step_machine_date_error = $second_step_machine_time_slots_error = "";
 
   echo "<pre>";
   echo "SESSIE";
@@ -39,31 +40,30 @@
         // set the reservation-item
         if (!empty($_POST["reservation-item"])) {
           $_SESSION["reservation"]["reservation-item"] = $reservation_item = $_POST["reservation-item"];
+        } else {
+          $first_step_error = "Kies een " . (($reservation_type === "workshop") ? $reservation_type : "toestel");
         }
 
-        // when reservation-item is not filled in and type is workshop
-        if ($reservation_type === "workshop") {
-          if (empty($reservation_item)) {
-            $first_step_ws_error = "Kies een workshop";
-          }
-        } else if ($reservation_type === "machine") {
+        if ($reservation_type === "machine") {
           if (!empty($_POST["reservation-date"])) {
             $_SESSION["reservation"]["reservation-date"] = $reservation_date = $_POST["reservation-date"];
-          }
-
-          if (empty($reservation_item)) {
-            $first_step_machine_error = "Kies een toestel";
-          }
-
-          if (empty($reservation_date)) {
+          } else {
             $first_step_machine_date_error = "Selecteer een datum";
           }
         }
       } // End of first step
+      else if ($current_step === 2) {
+        if (isset($_POST["reservation-time-slots"]) && count($_POST["reservation-time-slots"]) > 0) {
+          $_SESSION["reservation"]["reservation-time-slots"] = $reservation_time_slots = $_POST["reservation-time-slots"];
+        } else {
+          $second_step_machine_time_slots_error = "Selecteer één of meerdere tijdsloten voor uw reservatie";
+        }
+      } // End of second step
 
       if (
-        empty($init_step_error) && empty($first_step_ws_error)
-        && empty($first_step_machine_error) && empty($first_step_machine_date_error)
+        empty($init_step_error) && empty($first_step_error)
+        && empty($first_step_machine_date_error)
+        && empty($second_step_machine_time_slots_error)
       ) {
         // update the current step value
         $_SESSION["reservation"]["reservation-step"] = $current_step = $next_step;
@@ -153,7 +153,7 @@
       </pre>
       <div class="input-group">
         <label for="reservation-item">Welke workshop wilt u reserveren?</label>
-        <select id="reservation-item" name="reservation-item" class="<?php echo (!empty($first_step_ws_error)) ? "error" : ""; ?>">
+        <select id="reservation-item" name="reservation-item" class="<?php echo (!empty($first_step_error)) ? "error" : ""; ?>">
           <option value="" <?php echo ($reservation_item == "") ? "selected" : ""; ?>>-- Kiezen --</option>
           <?php
             $all_workshops = new WP_Query([
@@ -172,7 +172,7 @@
             }
           ?>
         </select>
-        <span class="error-message <?php echo ($first_step_ws_error !== "") ? 'disp-b' : 'disp-n'; ?>"><?php echo $first_step_ws_error; ?></span>
+        <span class="error-message <?php echo ($first_step_error !== "") ? 'disp-b' : 'disp-n'; ?>"><?php echo $first_step_error; ?></span>
       </div>
       <input type="hidden" name="step" value="3" />
       <div class="disp-f col-2-of-2">
@@ -194,7 +194,7 @@
       </pre>
       <div class="input-group">
         <label for="reservation-item">Welk toestel wilt u reserveren?</label>
-        <select id="reservation-item" name="reservation-item" class="<?php echo (!empty($first_step_machine_error)) ? "error" : ""; ?>">
+        <select id="reservation-item" name="reservation-item" class="<?php echo (!empty($first_step_error)) ? "error" : ""; ?>">
           <option value="" <?php echo ($reservation_item == "") ? "selected" : ""; ?>>-- Kiezen --</option>
           <?php
             $all_machines = new WP_Query([
@@ -213,8 +213,8 @@
             }
           ?>
         </select>
-        <span class="error-message <?php echo (!empty($first_step_machine_error)) ? 'disp-b' : 'disp-n'; ?>">
-          <?php echo $first_step_machine_error; ?>
+        <span class="error-message <?php echo (!empty($first_step_error)) ? 'disp-b' : 'disp-n'; ?>">
+          <?php echo $first_step_error; ?>
         </span>
       </div>
       <div class="input-group">
@@ -280,7 +280,7 @@
         <div class="time-slot">
           <p class="time-slot-title"><?php echo $current_time; ?></p>
           <label>
-            <input type="checkbox" name="time_slots[]" value="<?php echo $row; ?>" />
+            <input type="checkbox" name="reservation-time-slots[]" value="<?php echo $current_time; ?>" />
             reserveren
           </label>
         </div>
@@ -292,6 +292,9 @@
           }
         ?>
       </div>
+      <span class="error-message <?php echo (!empty($second_step_machine_time_slots_error)) ? "disp-b" : "disp-n"; ?>">
+        <?php echo $second_step_machine_time_slots_error; ?>
+      </span>
       <input type="hidden" name="step" value="3" />
       <div class="disp-f col-2-of-2">
         <div class="col-1-of-2">
