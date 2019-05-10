076ed0d888a7a2e99d0896ac9a1b3676b5b64262
carodeweze <caro_de_weze@hotmail.com>
2019-05-10 14:21:33 +0200

    css opnieuws nieuws

--- index.php
+++ index.php
@@ -7,49 +7,42 @@
     -> in the case of KdG Fablab: Nieuws
    */
 ?>
-<main id="mainToestellen"  class="mainNews disp-f">
-  <section role="news">
+<main id="mainToestellen"  class="disp-f">
+  <section role="machines">
   <h1>Laatste nieuws</h1>
   <article class="disp-f">
   <?php
     while(have_posts()) {
       the_post();
   ?>
-<div class="toestelContent newsContent">
-        <?php if (has_post_thumbnail()) { ?>
-     <a href="<?php the_permalink(); ?>" class="thumbnail">
-       <?php the_post_thumbnail(); ?>
-     </a>
-     <?php } else { ?>
-     <img
-      class="thumbnail"
-      src="<?php echo get_theme_file_uri("img/default_news.jpg"); ?>"
-      alt="news" />
-    <?php } ?>
-    <div class="content">
-
-
-    <h2 class="title">
-      <span role="accentuation">
-        <?php
-          $post_type = get_post_type();
-          $accent = "";
+<div class="toestelContent">
+    <div class="img-thumb">
+        <a href="<?php the_permalink(); ?>" class="thumbnail">
+        <?php if (has_post_thumbnail()) {
+          the_post_thumbnail();
+        } else {
         ?>
-      </span>
-      <?php echo strtoupper(get_the_title()); ?>
-    </h2>
-    <p class="excerpt">
-     <?php
-       if (has_excerpt()) {
-         echo get_the_excerpt();
-       } else {
-         echo wp_trim_words(get_the_content(), 25); // control number of words
-       }
-     ?>
-    </p>
-    <div class="buttons newsButtons p-rel">
-      <a class="btn btn-dark morenewsBtn " href="<?php the_permalink(); ?>">Lees verder</a>
+          <img class="valencia" src="<?php echo get_theme_file_uri("img/default_news.jpg"); ?>" alt="news">
+        <?php } ?>
+        </a>
     </div>
+    
+    <div class="content">
+        <h2 class="title">
+          <?php echo strtoupper(get_the_title()); ?>
+        </h2>
+        <p class="excerpt">
+         <?php
+           if (has_excerpt()) {
+             echo get_the_excerpt();
+           } else {
+             echo wp_trim_words(get_the_content(), 25); // control number of words
+           }
+         ?>
+        </p>
+        <div class="buttons disp-f col-2-of-2">
+          <a class="btn btn-dark " href="<?php the_permalink(); ?>">Lees verder</a>
+        </div>
 
     </div>
   </div>
