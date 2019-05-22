8cff423d5c6dcf3c36d5535fd5a07d4f3d9a9f81
carodeweze <caro_de_weze@hotmail.com>
2019-05-22 10:43:31 +0200

    html & css 404 pagina

--- 404.php
+++ 404.php
@@ -1,17 +1,16 @@
    <?php
    get_header(); ?>
-
-<main>
-  <div class="intro404 disp-f">
-    <img class="img_404" src="<?php echo get_theme_file_uri("img/iconStop.png"); ?>">
-    <div class="introContent_404">
-      <h1 class="page-title">Error404: Helaas kunnen we  de pagina die u zoekt niet vinden.</h1>
-      <p>Dit kan komen doordat de pagina niet meer bestaat, of doordat eeen typefout is gemaakt bij het invoeren van het adres. U kunt het via de navigatie de weg vinden.</p>
+   
+<main id="main404">
+    <div class="error404 disp-f">
+        <div class="img404">
+            <img class="img_404" src="<?php echo get_theme_file_uri("img/iconStop.png"); ?>">
+        </div>
+        <div class="tekst404">
+            <h1>Error 404: Pagina niet gevonden</h1>
+            <p>Helaas kunnen we de pagina die u zoekt niet vinden. Dit kan komen doordat de pagina niet meer bestaat, of doordat een typefout is gemaakt bij het invoeren van het adres.</p>
+        </div>
     </div>
-  </div>
 </main>
-
-
-
 
    <?php get_footer(); ?>
