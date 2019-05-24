f9e8c643a606fd89ab9196aea3866a7b69c18fa4
Katodebock <36281116+Katodebock@users.noreply.github.com>
2019-05-24 14:16:03 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,23 @@
     <footer>
       <?php /* footer content goes here */ ?>
       <div id="adres">
+        <h2>Adres</h2>
         <h1>KdG Fablab</h1>
         <p>Salesianenlaan 90</p>
         <p>2660 Hoboken</p>
       </div>
+      <div class="openingsuren">
+        <h2>Openingsuren</h2>
+        <?php $openinghours = get_option('kdg_fablab_rs_opening_hours');
+        var_dump($openinghours);
+        ?>
+        <ul>
+          <li>lalala</li>
+          <li>lololo</li>
+        </ul>
+      </div>
       <div id="socials">
+        <h2>Social Media</h2>
         <a href="https://www.facebook.com/kareldegrotehogeschool/">
           <img src="<?php echo get_theme_file_uri("img/fbWhite2.png"); ?>" alt="facebook icon">
         </a>
